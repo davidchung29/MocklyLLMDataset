895ef8f456c8f3e020965c10ce5575f2b4d347da
welu28 <27luw@millburn.org>
2025-07-08 12:54:06 -0400

    changed venv

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\luzij\OneDrive\Documents\GitHub\CSYA-Behavioral-Dataset\myenv
+set VIRTUAL_ENV=C:\Users\luzij\OneDrive\Documents\GitHub\MocklyLLMDataset\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
