ad87a08a4d1912f4690cde964e4b90017d65cc3d
welu28 <151058854+welu28@users.noreply.github.com>
2025-07-08 12:36:39 -0400

    Moved files from old repo

---
+++ pipeline/download_wav.py
@@ -0,0 +1,42 @@
+import csv
+import os
+import subprocess
+from yt_dlp import YoutubeDL
+
+input_csv = 'data/youtube_links.csv'
+output_folder = 'data/downloaded_audio'
+#ffmpeg_path = r'C:\ProgramData\chocolatey\bin\ffmpeg.exe' - this was b/c ffmpeg path was not added to myenv
+#os.makedirs(output_folder, exist_ok=True)
+
+#download audio only
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl': os.path.join(output_folder, '%(id)s.%(ext)s'),
+    'quiet': True,
+    'noplaylist': True, #REMINDER: change this when downloading playlist
+}
+
+with open(input_csv) as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        url = row['url']
+        print(f'Downloading: {url}')
+        with YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
+            webm_file = os.path.join(output_folder, f"{info['id']}.webm")
+            wav_file = os.path.join(output_folder, f"{info['id']}.wav") #converted
+
+            #command for webm --> wav
+            command = [
+                "ffmpeg",
+                "-i", webm_file,
+                "-vn", 
+                "-ar", "16000", #adjust for quality/file size
+                "-ac", "1",
+                "-acodec", "pcm_s16le",
+                wav_file
+            ]
+
+            subprocess.run(command, check=True)
+            print(f"Saved wav: {wav_file}")
+            os.remove(webm_file)
